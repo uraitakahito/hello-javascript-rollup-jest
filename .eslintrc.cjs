675155969d64a9e9b5461df840a0525b58db39c3
Takahito Urai <uraitakahito@gmail.com>
2024-12-25 18:26:05 +0900

    npm i -D eslint-plugin-jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -45,6 +45,31 @@ module.exports = {
         'no-magic-numbers': 'off',
       },
     },
+    {
+      /*
+       * IMPORTANT: `all configuration` is not recommended for production use
+       * because it changes with every minor and major version of ESLint. Use it at your own risk.
+       * https://eslint.org/docs/v8.x/use/configure/configuration-files#using-eslintall
+       */
+      extends: ['plugin:jest/all'],
+      files: ['test/**', 'test-jest/**'],
+      // You can omit the eslint-plugin- prefix
+      plugins: ['jest'],
+      rules: {
+        // https://eslint.org/docs/v8.x/rules/no-hooks
+        'jest/no-hooks': 'off',
+        // https://github.com/jest-community/eslint-plugin-jest/blob/v28.9.0/docs/rules/prefer-importing-jest-globals.md
+        'jest/prefer-importing-jest-globals': 'off',
+        // https://github.com/jest-community/eslint-plugin-jest/blob/v28.9.0/docs/rules/require-hook.md
+        'jest/require-hook': 'off',
+      },
+    },
+    {
+      files: ['jest.config.mjs'],
+      rules: {
+        'max-len': 'off',
+      },
+    },
   ],
 
   parserOptions: {
