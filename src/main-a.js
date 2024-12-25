ded5ba28c692387353dad84950b117e2492e3933
Takahito Urai <uraitakahito@gmail.com>
2024-12-25 11:35:32 +0900

    Remove commented-out code from main-a.js

--- src/main-a.js
+++ src/main-a.js
@@ -9,11 +9,3 @@ import add from './calculator';
 //
 const result = add(1, answer);
 console.log(result);
-
-// import add from './calculator';
-
-// //
-// // @rollup/plugin-node-resolve test
-// //
-// const result = add(1, 3);
-// console.log(result);
