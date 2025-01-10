ed352877bfb451622168308bef71ed4d62dee42d
Takahito Urai <uraitakahito@gmail.com>
2025-01-10 09:25:01 +0900

    Refactor Rollup configuration: consolidate into a single config file, remove debug and default configs, and enhance output formats with terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,44 @@
-/* eslint-disable import/extensions */
-import defaultConfig from './rollup.default.config.js';
-import debugConfig from './rollup.debug.config.js';
+// A Rollup plugin to convert CommonJS modules to ES6
+import commonjs from 'rollup-plugin-commonjs';
+// The @rollup/plugin-node-resolve plugin teaches Rollup how to find external modules.
+import resolve from '@rollup/plugin-node-resolve';
+// https://rollupjs.org/command-line-interface/#config-intellisense
+import { defineConfig } from 'rollup';
+// To generate a minified bundle with terser
+import terser from '@rollup/plugin-terser';
 
-export default (commandLineArgs) => {
-  // If the command line argument --config-debug | --configDebug is set, use the debug configuration
-  if (commandLineArgs.configDebug === true) {
-    return debugConfig;
-  }
-  return defaultConfig;
-};
+const config = defineConfig(
+  [
+    {
+      input: 'src/main-a.js', // conditionally required
+
+      output: [
+        {
+          file: 'dist/iife/bundle.min.js',
+          format: 'iife',
+          plugins: [terser()],
+          globals: {
+            'the-answer': 'theAnswer',
+          },
+        },
+        {
+          dir: 'dist/es6',
+          format: 'es',
+        },
+        {
+          dir: 'dist/umd',
+          format: 'umd',
+          globals: {
+            'the-answer': 'theAnswer',
+          },
+        },
+      ],
+      plugins: [
+        commonjs(),
+        resolve(),
+      ],
+    },
+  ],
+);
+
+export default config;
